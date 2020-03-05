ed2b52edda17075224df663ad8ebe95a5f21119d
user <invulnarble@gmail.com>
2020-03-05 21:05:45 +0200

    finished

--- Lesson_3/app.js
+++ Lesson_3/app.js
@@ -13,6 +13,8 @@
 
 // console.log(sumMin([[1,2,3],[33,22,55],[16,42,345,79,1]]));
 
+
+
 // Task 2:Two Oldest Ages
 
 // function twoOldestAges(ages) {
@@ -24,6 +26,7 @@
 
 
 // Task 3: Shortest Word
+
 // function shortestWord(str) {
 //     let arr = str.split(' ').sort((a, b) => a.length - b.length);
 
@@ -33,6 +36,31 @@
 // };
 
 // console.log(shortestWord('i want to travel the world writing code one day'));
+
+
+
+// Task 4: Your order, please
+
+// function yourOrder(str) {
+//     if(str === ""){
+//       return "";
+//     }
+
+//     let result = [];
+//     str = str.split(" ");
+
+//     for(let i = 0; i < str.length; i++){
+//         for(let j = 0; j < str.length; j++){  
+//             if(str[j].indexOf(i) > -1){
+//                 result.push(str[j]);
+//             }};
+//     };
+
+//     return result.join(" ");
+  
+//   };
+
+//   console.log(yourOrder('4of Fo1r pe6ople g3ood th5e the2'));
 
 // Task 5
 
